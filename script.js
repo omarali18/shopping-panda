c84df2288ec34cfd37947aa5adf3d848fdffe2d1
omarali18 <omarali1814000@gmail.com>
2021-08-14 22:52:47 +0600

    js first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// All h2 lightblue color.
+const allH2 = document.getElementsByTagName("h2");
+for (const h2 of allH2) {
+    h2.style.color = "lightblue"
+}
+// Backpack section background color set.
+document.getElementById("Backpack-section").style.backgroundColor = "tomato"
