30d1eac3be99937458c247b5a6e6d7bc09b83af4
omarali18 <omarali1814000@gmail.com>
2021-08-14 23:02:27 +0600

    js second commit

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ for (const h2 of allH2) {
     h2.style.color = "lightblue"
 }
 // Backpack section background color set.
-document.getElementById("Backpack-section").style.backgroundColor = "tomato"
+document.getElementById("Backpack-section").style.backgroundColor = "tomato";
+// Card border radious add.
+const allCard = document.getElementsByClassName("card");
+for (const card of allCard) {
+    console.log(card);
+    card.style.borderRadius = "30px"
+};
